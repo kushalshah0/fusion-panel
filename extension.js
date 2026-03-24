28910707dc85c867a201f076a9ce07ede720d729
Kushal Shah <code@kushalshah.com.np>
2026-03-24 17:17:14 +0545

    Update extension entry point with new modules integration

--- extension.js
+++ extension.js
@@ -1,41 +1,36 @@
 import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js';
 import { PanelManager } from './panel/panelManager.js';
-import { BlurManager } from './blur/blurManager.js';
 import { SettingsManager } from './utils/settings.js';
 
 export default class FusionPanelExtension extends Extension {
     enable() {
         log('[Fusion Panel] Enabling...');
 
-        // Initialize settings
-        this._settings = new SettingsManager(this.dir);
+        try {
+            this._settings = new SettingsManager(this.dir);
+            this._panelManager = new PanelManager(this._settings);
+            this._panelManager.enable();
 
-        // Create and enable the panel
-        this._panelManager = new PanelManager(this._settings);
-        this._panelManager.enable();
-
-        // Create and enable blur
-        this._blurManager = new BlurManager(this._settings);
-        this._blurManager.enable(this._panelManager.getPanelActor());
-
-        log('[Fusion Panel] Enabled successfully!');
+            log('[Fusion Panel] Enabled successfully!');
+        } catch (e) {
+            log(`[Fusion Panel] Error enabling: ${e.message}`);
+            log(e.stack);
+        }
     }
 
     disable() {
         log('[Fusion Panel] Disabling...');
 
-        // Disable blur first
-        this._blurManager?.disable();
-        this._blurManager = null;
+        try {
+            this._panelManager?.disable();
+            this._panelManager = null;
 
-        // Then disable panel (restores original GNOME panel)
-        this._panelManager?.disable();
-        this._panelManager = null;
+            this._settings?.destroy();
+            this._settings = null;
 
-        // Clean up settings
-        this._settings?.destroy();
-        this._settings = null;
-
-        log('[Fusion Panel] Disabled.');
+            log('[Fusion Panel] Disabled.');
+        } catch (e) {
+            log(`[Fusion Panel] Error disabling: ${e.message}`);
+        }
     }
 }
